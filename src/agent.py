4fdc83013c3e87259ecbeadca5785afed1e79880
vikiscience <victoria.bibaeva@yandex.com>
2020-05-12 23:08:09 +0200

    refactor, change act method to process 2 states and return 2 actions

--- src/agent.py
+++ src/agent.py
@@ -19,14 +19,14 @@ class DRLAgent:
                  batch_size: int = const.batch_size,
                  expl_noise: int = const.expl_noise,
                  model_learning_rate: float = const.model_learning_rate,
-                 num_fc_actor: int = const.num_fc_actor,
-                 num_fc_critic: int = const.num_fc_critic
+                 num_fc_1: int = const.num_fc_1,
+                 num_fc_2: int = const.num_fc_2
                  ):
 
         # agent params
         self.num_states = num_states
         self.num_actions = num_actions
-        self.memory = buffer.ReplayBuffer(memory_size)
+        self.memory = buffer.ReplayBuffer(memory_size)  # todo shared buffer for both agents
         self.gamma = gamma
         self.batch_size = batch_size
         self.expl_noise = expl_noise
@@ -34,30 +34,29 @@ class DRLAgent:
 
         # model params
         self.model_learning_rate = model_learning_rate
-        self.num_fc_actor = num_fc_actor
-        self.num_fc_critic = num_fc_critic
+        self.num_fc_1 = num_fc_1
+        self.num_fc_2 = num_fc_2
 
-        self.policy = models.TD3(state_dim=self.num_states,
-                                 action_dim=self.num_actions,
-                                 max_action=const.max_action,
-                                 discount=self.gamma,
-                                 num_fc_actor=self.num_fc_actor,
-                                 num_fc_critic=self.num_fc_critic,
-                                 learning_rate=self.model_learning_rate)
+        self.policy = models.Network(state_dim=self.num_states,
+                                     action_dim=self.num_actions,
+                                     num_fc_1=self.num_fc_1,
+                                     num_fc_2=self.num_fc_2)
 
-        self._model_summary(self.policy.actor, title='Actor')
-        self._model_summary(self.policy.critic, title='Critic')
+        self._model_summary(self.policy, title='Network')
 
     def act(self, states, t):
         # Select action randomly or according to policy
         if t < self.start_policy_training_iter:
-            action = np.random.randn(const.num_agents, self.num_actions)
+            actions = np.random.randn(const.num_agents, self.num_actions)
         else:
-            action = (
-                    self.policy.select_action(np.array(states[0]))  # todo [0]
-                    + np.random.normal(0, const.max_action * self.expl_noise, size=self.num_actions)
-            )
-        actions = action.reshape(1, -1)
+            actions = []
+            for s in states:
+                a = self.policy(torch.Tensor(s))[0].cpu().data.numpy()  # policy returns (action, value) --> use index 0
+                noise = np.random.normal(0, const.max_action * self.expl_noise, size=self.num_actions)
+                actions.append(a + noise)
+
+            actions = np.array(actions)
+
         actions = np.clip(actions, - const.max_action, const.max_action)  # all actions between -1 and 1
 
         return actions
@@ -65,42 +64,32 @@ class DRLAgent:
     def do_stuff(self, state, action, reward, next_state, done, t):
         self.memory.append((state, action, reward, next_state, done))  # memorize
 
+        # todo
         # Train agent after collecting sufficient data
-        if t >= self.start_policy_training_iter:
-            self.policy.train(self.memory, self.batch_size)
+        # if t >= self.start_policy_training_iter:
+        #     self.policy.train(self.memory, self.batch_size)
 
     def load(self):
         const.myprint('Loading model from:', self.model_path)
         # load with architecture
         checkpoint = torch.load(self.model_path)
-        self.policy = models.TD3(state_dim=checkpoint['num_states'],
-                                 action_dim=checkpoint['num_actions'],
-                                 max_action=const.max_action,
-                                 discount=checkpoint['gamma'],
-                                 num_fc_actor=checkpoint['num_fc_actor'],
-                                 num_fc_critic=checkpoint['num_fc_critic'],
-                                 learning_rate=checkpoint['learning_rate'])
-        self.policy.critic.load_state_dict(checkpoint['critic'])
-        self.policy.critic_optimizer.load_state_dict(checkpoint['critic_optimizer'])
-        self.policy.actor.load_state_dict(checkpoint['actor'])
-        self.policy.actor_optimizer.load_state_dict(checkpoint['actor_optimizer'])
+        self.policy = models.Network(state_dim=checkpoint['num_states'],
+                                     action_dim=checkpoint['num_actions'],
+                                     num_fc_1=checkpoint['num_fc_1'],
+                                     num_fc_2=checkpoint['num_fc_2'])
+        self.policy.load_state_dict(checkpoint['network'])
 
         # change mode (to use only for inference)
-        self.policy.actor.eval()
+        self.policy.eval()
 
     def save(self):
         const.myprint('Saving model to:', self.model_path)
         # save with architecture
         checkpoint = {'num_states': self.num_states,
                       'num_actions': self.num_actions,
-                      'gamma': self.gamma,
-                      'num_fc_actor': self.num_fc_actor,
-                      'num_fc_critic': self.num_fc_critic,
-                      'learning_rate': self.model_learning_rate,
-                      'critic': self.policy.critic.state_dict(),
-                      'critic_optimizer': self.policy.critic_optimizer.state_dict(),
-                      'actor': self.policy.actor.state_dict(),
-                      'actor_optimizer': self.policy.actor_optimizer.state_dict()
+                      'num_fc_1': self.num_fc_1,
+                      'num_fc_2': self.num_fc_2,
+                      'network': self.policy.state_dict()
                       }
         torch.save(checkpoint, self.model_path)
 
