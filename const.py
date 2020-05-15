6a7ba2df94bd12219b8130e0e524c4f583b18c92
vikiscience <victoria.bibaeva@yandex.com>
2020-05-15 14:29:21 +0200

    newest hyperparams

--- const.py
+++ const.py
@@ -12,27 +12,27 @@ file_path_ref_img_score = output_path / 'score_ref.png'
 # general params
 random_seed = 0xABCD
 rolling_mean_N = 100
+num_episodes_test = 10
 num_agents = 2
 state_size = 24
 action_size = 2
 max_action = 1.
-episode_length = 300
 verbose = False
 high_score = 0.5
 
 # algo params
-num_episodes = 250
+num_episodes = 5000
 
 # agent params
-memory_size = 40000
+memory_size = 200000
 gamma = 0.95
 batch_size = 32
 expl_noise = 0.3
 
 # model params
-model_learning_rate = 0.001
-num_fc_1 = 64
-num_fc_2 = 32
+model_learning_rate = 0.00001
+num_fc_1 = 16
+num_fc_2 = 16
 
 
 def myprint(*args, **kwargs):
