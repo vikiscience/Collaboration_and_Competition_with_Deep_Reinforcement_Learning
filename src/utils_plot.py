25a54c7b5037a98d012b2a60b25514e9789a49c7
vikiscience <victoria.bibaeva@yandex.com>
2020-05-15 15:24:05 +0200

    change score plot from line to scatter on demand

--- src/utils_plot.py
+++ src/utils_plot.py
@@ -4,7 +4,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 
-def plot_history_rolling_mean(hist, N=const.rolling_mean_N, fp=const.file_path_img_score):
+def plot_history_rolling_mean(hist, N=const.rolling_mean_N, fp=const.file_path_img_score, use_scatter=True):
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
@@ -12,14 +12,19 @@ def plot_history_rolling_mean(hist, N=const.rolling_mean_N, fp=const.file_path_i
     x = pd.Series(hist)
     y = x.rolling(window=N).mean().iloc[N - 1:]
 
-    # plot
-    plt.plot(hist, c='darkorchid', marker='.', markevery=[-1])
-    plt.plot(y, c='blue', marker='.', markevery=[-1])
+    # plot scores for each iteration (history)
+    if use_scatter:
+        plt.scatter(x=range(len(hist)), y=hist, c='darkorchid', s=2, label='score')
+    else:
+        plt.plot(hist, c='darkorchid', marker='.', markevery=[-1], label='score')
+
+    # plot rolling mean scores
+    plt.plot(y, c='blue', marker='.', markevery=[-1], label='rolling_score (N={})'.format(N))
 
     # plot line to signify the aimed high score
     x1, x2 = 0, len(hist)
     y1, y2 = const.high_score, const.high_score
-    plt.plot([x1, x2], [y1, y2], color='k', linestyle='--', linewidth=1)
+    plt.plot([x1, x2], [y1, y2], color='k', linestyle='--', linewidth=1, label='high_score')
 
     # annotate last_history and last_rolling point
     last_points = [(len(hist) - 1, hist[-1])]
@@ -27,11 +32,11 @@ def plot_history_rolling_mean(hist, N=const.rolling_mean_N, fp=const.file_path_i
         last_points.append((len(hist) - 1, y.iloc[-1]))
 
     for (i, j) in last_points:
-        ax.annotate('{:.2f}'.format(j), xy=(i, j), xytext=(i + 0.1, j))
+        ax.annotate('{:.3f}'.format(j), xy=(i, j), xytext=(i + 0.1, j))
 
     plt.xlabel('Episodes')
     plt.ylabel('Score (Sum of Rewards)')
     plt.title('Online Performance')
-    plt.legend(['score', 'rolling_score (N={})'.format(N)], loc='best')
+    plt.legend(loc='best')
     plt.savefig(fp)
     plt.close()
