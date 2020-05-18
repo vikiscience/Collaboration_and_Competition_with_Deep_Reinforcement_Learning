4f737a3908a448e41976c56718efb553caacd1df
vikiscience <victoria.bibaeva@yandex.com>
2020-05-18 22:23:24 +0200

    new params and values

--- const.py
+++ const.py
@@ -8,6 +8,7 @@ file_path_model = model_path / 'model.npy'
 file_path_ref_model = model_path / 'model_ref.npy'
 file_path_img_score = output_path / 'score.png'
 file_path_ref_img_score = output_path / 'score_ref.png'
+file_path_img_loss = output_path / 'loss.png'
 
 # general params
 random_seed = 0xABCD
@@ -16,23 +17,25 @@ num_episodes_test = 10
 num_agents = 2
 state_size = 24
 action_size = 2
-max_action = 1.
 verbose = False
 high_score = 0.5
 
 # algo params
-num_episodes = 5000
+num_episodes = 1000
+max_action = 1.
 
 # agent params
 memory_size = 200000
-gamma = 0.95
-batch_size = 32
+gamma = 0.99
+batch_size = 128
 expl_noise = 0.3
+tau = 0.06  # 0.01
+policy_freq = 1  # 2
 
 # model params
-model_learning_rate = 0.00001
-num_fc_1 = 16
-num_fc_2 = 16
+model_learning_rate = 0.001  # 0.00001
+num_fc_1 = 256  # 16
+num_fc_2 = 128  # 16
 
 
 def myprint(*args, **kwargs):
